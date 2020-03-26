f175c66b10e5ae58de041d6f58cbb8777068cf5e
cselmi <rm@172.20.10.7>
2020-03-26 13:14:07 +0100

    2020.03.26 Aggiunte librerie di Lorenzo

--- Librerie/mask.py
+++ Librerie/mask.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Librerie.region_of_interest import RegionOfInterest
 
 __version__ = "$Id: $"
 
@@ -79,26 +80,3 @@ class CircularMask():
         radius= int(self.radius())
         return RegionOfInterest(centerX - radius, centerX + radius,
                                 centerY - radius, centerY + radius)
-
-
-class RegionOfInterest(object):
-
-    def __init__(self, xmin, xmax, ymin, ymax):
-        self.xmin= int(xmin)
-        self.xmax= int(xmax)
-        self.ymin= int(ymin)
-        self.ymax= int(ymax)
-
-
-    def toNumpyArray(self):
-        return np.array([self.xmin, self.xmax, self.ymin, self.xmax])
-
-
-    def __repr__(self):
-        return "[%d:%d, %d:%d]" % (self.xmin, self.xmax, self.ymin, self.xmax)
-
-
-    def cutOut(self, frame):
-        return frame[self.ymin: self.ymax, self.xmin: self.xmax]
-
-
