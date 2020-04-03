8c574afcf79cd36a9f76dca8ebbdd01362b34637
cselmi <rm@172.20.10.7>
2020-04-03 17:58:40 +0200

    2020.04.03 Chiara: img_redux e quello che serve per farlo funzionare

---
+++ Chiara/configuration.py
@@ -0,0 +1,91 @@
+'''
+@author: cs
+'''
+import numpy as np
+import os
+
+
+class Configuration():
+    '''
+    Constants of system configuration
+    '''
+    #ROOT IN MY PERSONAL PC
+    LOG_ROOT_FOLDER = '/Users/rm/Desktop/Arcetri/M4/ProvaCodice'
+
+    M4COORDINATE_ROOT_FOLDER = \
+        '/Users/rm/Desktop/Arcetri/M4/ActuatorCoordinates.fits'
+
+    CALIBRATION_ROOT_FOLDER = "/Users/rm/Desktop/Arcetri/M4"
+
+    OPD_DATA_FOLDER = "/Users/rm/Desktop/Arcetri/M4/ProvaCodice"
+
+    TEST_IMAGE_ROOT_FOLDER = \
+        "/Users/rm/Desktop/Arcetri/M4/ProvaCodice/Immagini_prova"
+
+    IFFUNCTIONS_ROOT_FOLDER = \
+        "/Users/rm/Desktop/Arcetri/M4/ProvaCodice/IFFunctions"
+
+    V_MATRIX_FOR_SEGMENT_ROOT_811= \
+        '/Users/rm/Desktop/Arcetri/M4/ProvaCodice/IFFunctions/20170630_105105/modeMatrix.fits'
+
+    V_MATRIX_FOR_SEGMENT_ROOT_892= \
+        '/Users/rm/Desktop/Arcetri/M4/ProvaCodice/IFFunctions/20170216_123645/modeMatrix.fits'
+
+
+    #ROOT IN M4 PC
+#     CALIBRATION_ROOT_FOLDER = '/mnt/shared/M4Data'
+#  
+#     OPD_DATA_FOLDER = os.path.join(CALIBRATION_ROOT_FOLDER, 'OPTData')
+#  
+#     LOG_ROOT_FOLDER = os.path.join(CALIBRATION_ROOT_FOLDER,  'LOGData/mylog')
+#  
+#     M4COORDINATE_ROOT_FOLDER = \
+#         os.path.join(CALIBRATION_ROOT_FOLDER, 'CONFData/20191210_144400/ActuatorCoordinates.fits')
+#  
+#     V_MATRIX_FOR_SEGMENT_ROOT_811= \
+#         os.path.join(CALIBRATION_ROOT_FOLDER, 'CONFData/20191210_144400/ff_v_matrix.fits')
+#  
+#     IFFUNCTIONS_ROOT_FOLDER = \
+#         os.path.join(OPD_DATA_FOLDER, 'IFFunctions')
+
+    #SPL
+    TN_FRINGES = '20181108_1'
+    #Interf
+    LAMBDA = 2
+    #Ott
+    #ParabolaPupilXYRadius= np.array([128, 128, 128]) #centerX, centerY, radius
+    PARABOLA_PUPIL_XYRADIUS = np.array([256, 256, 256])
+    PARABOLA_DOF = 3
+    RM_DOF = 2
+    PIXEL_SCALE = 360.5 #PIXEL/METRI
+    #DM
+    M4_MECHANICAL_PUPIL_XYRADIUS = np.array([458, 458, 458]) #np.array([512, 512, 512])
+    M4_OPTICAL_DIAMETER = 858
+    N_SEG = 6
+    N_ACTS_TOT = 5352
+    N_ACT_SEG = 892
+    M4_DOF = 6
+    REFERENCE_ANGLE_RAD = np.pi / 3
+    REFERENCE_ANGLE_DEGREES = 60
+    SEGMENT_DISTANCE_FROM_CENTRE = 320
+    DIAMETER_IN_PIXEL_FOR_SEGMENT_IMAGES = 512
+    BIG_IMAGE_DIAMETER = 1236
+
+    #per l'applicazione
+    def set_up_logger(self, file_path, logging_level):
+        """ Set up logger for the application """
+        import logging
+        import logging.handlers
+        FORMAT = '%(asctime)s %(levelname)s %(name)s %(message)s'
+        f = logging.Formatter(fmt = FORMAT)
+        handler = logging.handlers.RotatingFileHandler(file_path,
+                                                       encoding='utf8',
+                                                       maxBytes=10000,
+                                                       backupCount=3)
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging_level)
+        handler.setFormatter(f)
+        handler.setLevel(logging_level)
+        root_logger.addHandler(handler)
+        handler.doRollover()
+    
---
+++ Chiara/roi.py
@@ -0,0 +1,75 @@
+'''
+@author: cs
+'''
+
+import logging
+import numpy as np
+from skimage.draw import circle
+from scipy import ndimage as ndi
+from Chiara.configuration import Configuration
+
+
+class ROI():
+    """
+    Class to be used for extracting regions of interest from the image.
+
+    HOW TO USE IT:
+    from m4.utils.roi import ROI
+    roi= ROI()
+    """
+
+    def __init__(self):
+        """The constructor """
+        self._logger = logging.getLogger('ROI:')
+        self._bigDiameter = Configuration.BIG_IMAGE_DIAMETER
+        self._segmentImaDiameter = Configuration.DIAMETER_IN_PIXEL_FOR_SEGMENT_IMAGES
+
+
+    def roiGenerator(self, ima):
+        '''
+        arg:
+            ima = image (np.masked_array)
+
+        return:
+            roiList = list of the first 12 roi found in the image
+
+        NOTA: roiList[3] = RM roi for alignement
+              roiList[3] = central roi for segment
+
+        '''
+        self._logger.info('Creation of roi list')
+        labels = ndi.label(np.invert(ima.mask))[0]
+        #import skimage.morphology as skm
+        #pro= skm.watershed(ima, markers)
+        roiList = []
+        for i in range(1, 13):
+            maski = np.zeros(labels.shape, dtype=np.bool)
+            maski[np.where(labels == i)] = 1
+            final_roi = np.ma.mask_or(np.invert(maski), ima.mask)
+            roiList.append(final_roi)
+        return roiList
+
+    def create_circular_mask(self, center_y, center_x, radius, imagePixels=None):
+        if imagePixels is None:
+            imagePixels = 512
+        else:
+            imagePixels = imagePixels
+        mask = np.ones((imagePixels, imagePixels), dtype= bool)
+        rr, cc = circle(center_y, center_x, radius)
+        mask[rr,cc] = 0
+        return mask
+
+    def circularMaskForSegmentCreator(self):
+        center_y = self._bigDiameter / 2
+        center_x = self._bigDiameter / 2
+        radius = Configuration.M4_OPTICAL_DIAMETER / 2
+        big_mask = self.create_circular_mask(center_y,
+                                         center_x, radius, self._bigDiameter)
+
+        seg_center_y = np.int(self._bigDiameter/2 + Configuration.SEGMENT_DISTANCE_FROM_CENTRE)
+        seg_center_x = np.int(self._bigDiameter/2)
+        seg_radius = np.int(self._segmentImaDiameter / 2)
+        mask = big_mask[seg_center_y - seg_radius : seg_center_y + seg_radius,
+                        seg_center_x - seg_radius : seg_center_x + seg_radius]
+        return mask
+        
